55de65d49f95001466d847bffd62c621d37f5602
Chris Foster <fosterchris22@gmail.com>
2017-03-04 12:35:23 -0500

    Add basic firing.

--- script.js
+++ script.js
@@ -9,10 +9,22 @@ var movement = {
 	movingRight: false
 }
 
-var currentlyMoving;
+var mouseX, mouseY;
 
 document.addEventListener("keydown", function(event) {
-	determineMovement(event.keyCode)
+	var key = event.keyCode;
+	if (key < 41) {
+		determineMovement(key);
+	}
+	else {
+		attack();
+	}
+});
+
+document.addEventListener("mousemove", function(event) {
+	mouseX = event.pageX;
+	mouseY = event.pageY;
+	console.log(mouseX, mouseY);
 });
 
 document.addEventListener("keyup", function(event) {
@@ -77,6 +89,37 @@ function determineMovement(key) {
 function move(direction, value, movementAction) {
 		var directionValue = parseInt(hero.style[direction].split("px")[0]);
 		directionValue += value;
-		console.log(directionValue)
 		hero.style[direction] = directionValue + "px";
+}
+
+
+
+
+function attack() {
+	// var beginLeft = hero.style.left;
+	// var beginTop = hero.style.top;
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	var left = parseInt(hero.style["left"].split("px")[0]);
+	var top = parseInt(hero.style["top"].split("px")[0]);
+	var random = Math.floor(Math.random() * 1000000);
+	var classRandom = 'fired' + random;
+	var bulletRandom = 'bullet' + random;
+	style.innerHTML = '.' + bulletRandom + ' { position: fixed; left: ' + (left) + 'px; top: ' + top + 'px; } ' + '.' + classRandom + ' { left: ' + (mouseX * 4)  + 'px; top: ' + (mouseY  * 4) + 'px; }';
+	document.getElementsByTagName('head')[0].appendChild(style);
+
+	var bullet = document.createElement("div");
+	bullet.classList.add("bullet", bulletRandom);
+	// bullet.style.top = beginTop;
+	// console.log(bullet.style)
+	hero.prepend(bullet);
+	// appendedBullet = document.getElementsByClassName("bullet")[0];
+
+
+	setTimeout(function() {
+		bullet.classList.add(classRandom)
+	}, 20)
+
+	// appendedBullet.style.left = "100px";
+	// console.log(appendedBullet.style)
 }
